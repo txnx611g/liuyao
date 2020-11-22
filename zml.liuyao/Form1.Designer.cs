47ed5f77901b02269afb5daac52084e3d7228990
tdnx611g <799298808@qq.com>
2020-11-22 12:43:13 +0800

    添加项目文件。

---
+++ zml.liuyao/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace zml.liuyao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(54, 158);
+            this.label1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "上";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(54, 98);
+            this.label2.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "下";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(54, 223);
+            this.label3.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "变";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnCalc.Location = new System.Drawing.Point(5, 287);
+            this.btnCalc.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(80, 33);
+            this.btnCalc.TabIndex = 3;
+            this.btnCalc.Text = "计算";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(85, 88);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 31);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(85, 148);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(138, 31);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(85, 213);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(138, 31);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBox3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox3_KeyDown);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(237, 98);
+            this.label4.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 21);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(237, 158);
+            this.label5.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 21);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(237, 223);
+            this.label6.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 21);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "label6";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnClear.Location = new System.Drawing.Point(141, 287);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(82, 33);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "清空";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(116, 259);
+            this.label7.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 21);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "label7";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox4.Location = new System.Drawing.Point(12, 42);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(357, 29);
+            this.textBox4.TabIndex = 12;
+            this.textBox4.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox4_KeyDown);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(8, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(82, 21);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "label8";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnSave.Location = new System.Drawing.Point(267, 287);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(82, 33);
+            this.btnSave.TabIndex = 14;
+            this.btnSave.Text = "保存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(251, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(98, 21);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "输入问题";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(365, 328);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "作者:周明龙  V0.66 2020/11/21";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ zml.liuyao/Form1.cs
@@ -0,0 +1,435 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zml.liuyao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Focus();
+            textBox4.Text =  "明天涨跌走势如何";
+            label8.Text= DateTime.Now.ToString();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            int upRemain = Convert.ToInt32(textBox1.Text) % 8;
+
+            if (upRemain == 0)
+            {
+                upRemain = 8;
+            }
+
+            label4.Text = upRemain.ToString();
+
+            upRemain = Convert.ToInt32(textBox2.Text) % 8;
+
+            if (upRemain == 0)
+            {
+                upRemain = 8;
+            }
+
+            label5.Text = upRemain.ToString();
+
+
+            upRemain = Convert.ToInt32(textBox3.Text) % 6;
+
+            if (upRemain == 0)
+            {
+                upRemain = 6;
+            }
+
+            label6.Text = upRemain.ToString();
+
+            if (label5.Text == "1")
+            {
+                switch (label4.Text )
+                {
+                    case "1":
+                        label7.Text = "乾为天190";
+                        break;
+                    case "2":
+                        label7.Text = "天泽履224";
+                        break;
+                    case "3":
+                        label7.Text = "天火同人235";
+                        break;
+                    case "4":
+                        label7.Text = "天雷无妄275";
+                        break;
+                    case "5":
+                        label7.Text = "天风姤343";
+                        break;
+                    case "6":
+                        label7.Text = "天水讼209";
+                        break;
+                    case "7":
+                        label7.Text = "天山遁306";
+                        break;
+                    case "8":
+                        label7.Text = "天地否231";
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+           else if (label5.Text == "2")
+            {
+                switch (label4.Text)
+                {
+                    case "1":
+                        label7.Text = "泽天夬224";
+                        break;
+                    case "2":
+                        label7.Text = "兑为泽389";
+                        break;
+                    case "3":
+                        label7.Text = "泽火革359";
+                        break;
+                    case "4":
+                        label7.Text = "泽雷随249";
+                        break;
+                    case "5":
+                        label7.Text = "泽风大过296";
+                        break;
+                    case "6":
+                        label7.Text = "泽水困352";
+                        break;
+                    case "7":
+                        label7.Text = "泽山咸299";
+                        break;
+                    case "8":
+                        label7.Text = "泽地萃346";
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+           else if (label5.Text == "3")
+            {
+                switch (label4.Text)
+                {
+                    case "1":
+                        label7.Text = "火天大有238";
+                        break;
+                    case "2":
+                        label7.Text = "火泽睽359";
+                        break;
+                    case "3":
+                        label7.Text = "离为火295";
+                        break;
+                    case "4":
+                        label7.Text = "火雷噬嗑261";
+                        break;
+                    case "5":
+                        label7.Text = "火风鼎362";
+                        break;
+                    case "6":
+                        label7.Text = " 火水未济412";
+                        break;
+                    case "7":
+                        label7.Text = "火山旅383";
+                        break;
+                    case "8":
+                        label7.Text = "火地晋312";
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+           else if (label5.Text == "4")
+            {
+                switch (label4.Text)
+                {
+                    case "1":
+                        label7.Text = "雷天大壮296";
+                        break;
+                    case "2":
+                        label7.Text = "雷泽归妹375";
+                        break;
+                    case "3":
+                        label7.Text = "雷火丰379";
+                        break;
+                    case "4":
+                        label7.Text = "震为雷365";
+                        break;
+                    case "5":
+                        label7.Text = "雷风恒302";
+                        break;
+                    case "6":
+                        label7.Text = "雷水解329";
+                        break;
+                    case "7":
+                        label7.Text = "雷山大过404";
+                        break;
+                    case "8":
+                        label7.Text = "雷地豫246";
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+           else if (label5.Text == "5")
+            {
+                switch (label4.Text)
+                {
+                    case "1":
+                        label7.Text = "风天小畜221";
+                        break;
+                    case "2":
+                        label7.Text = "风泽中孚400";
+                        break;
+                    case "3":
+                        label7.Text = "风火家人319";
+                        break;
+                    case "4":
+                        label7.Text = "风雷益337";
+                        break;
+                    case "5":
+                        label7.Text = "巽为风386";
+                        break;
+                    case "6":
+                        label7.Text = "风水涣395";
+                        break;
+                    case "7":
+                        label7.Text = "风山渐372";
+                        break;
+                    case "8":
+                        label7.Text = "风地观258";
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+           else if (label5.Text == "6")
+            {
+                switch (label4.Text)
+                {
+                    case "1":
+                        label7.Text = "水天需205";
+                        break;
+                    case "2":
+                        label7.Text = "水泽节397";
+                        break;
+                    case "3":
+                        label7.Text = "水火未济408";
+                        break;
+                    case "4":
+                        label7.Text = "水雷屯198";
+                        break;
+                    case "5":
+                        label7.Text = "水风井355";
+                        break;
+                    case "6":
+                        label7.Text = "坎为水291";
+                        break;
+                    case "7":
+                        label7.Text = "水山蹇325";
+                        break;
+                    case "8":
+                        label7.Text = "水地比217";
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+           else  if (label5.Text =="7")
+            {
+                switch (label4.Text)
+                {
+                    case "1":
+                        label7.Text = "山天大有279";
+                        break;
+                    case "2":
+                        label7.Text = "山泽损334";
+                        break;
+                    case "3":
+                        label7.Text = "山火贲264";
+                        break;
+                    case "4":
+                        label7.Text = "山雷颐282";
+                        break;
+                    case "5":
+                        label7.Text = "山风蛊252";
+                        break;
+                    case "6":
+                        label7.Text = "山水蒙201";
+                        break;
+                    case "7":
+                        label7.Text = "艮为山368";
+                        break;
+                    case "8":
+                        label7.Text = "山地剥268";
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+           else if (label5.Text == "8")
+            {
+                switch (label4.Text)
+                {
+                    case "1":
+                        label7.Text = "地天泰228";
+                        break;
+                    case "2":
+                        label7.Text = "地泽临255";
+                        break;
+                    case "3":
+                        label7.Text = "地火明夷315";
+                        break;
+                    case "4":
+                        label7.Text = "地雷复271";
+                        break;
+                    case "5":
+                        label7.Text = "地风升349";
+                        break;
+                    case "6":
+                        label7.Text = "地水师213";
+                        break;
+                    case "7":
+                        label7.Text = "地山谦242";
+                        break;
+                    case "8":
+                        label7.Text = "坤为地196";
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+           btnCalc.Focus();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            label4.Text = label5.Text = label6.Text = null;
+            label7.Text = null;
+            textBox1.Text = textBox2.Text = textBox3.Text = null;
+            textBox4.Text = "明天涨跌走势如何";
+            label8.Text = DateTime.Now.ToString();
+            textBox1.Focus();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label4.Text = label5.Text = label6.Text = "0";
+            label7.Text = null;
+
+        }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 3)
+            {
+                textBox2.Focus();
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Length == 3)
+            {
+                textBox3.Focus();
+            }
+
+        }
+        //变爻输入框
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox3.Text.Length == 3)
+            {
+                btnCalc.Focus();
+            }
+
+        }
+        //求测事件输入框
+        private void textBox4_KeyDown(object sender, KeyEventArgs e)
+        {
+                if (e.KeyCode == Keys.Enter)//如果输入的是回车键  
+                {
+                     textBox1.Focus();
+                }
+        }
+        //变爻按下回车键
+        private void textBox3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)//如果输入的是回车键  
+            {
+                this.btnCalc_Click(sender, e);
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            textBox1.TabIndex = 0;
+            textBox1.Focus();
+        }
+        /*保存卦*/
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog SavaData = new SaveFileDialog();                                                          
+            SavaData.Title = "保存文件";                        
+            SavaData.InitialDirectory =@"D:\断卦";               
+            SavaData.Filter = "TXT文件(*.txt)|*.txt";
+            SavaData.FileName = DateTime.Now.ToLongDateString().ToString() + textBox4.Text;
+            /*用户可能还没有计算,错误操作点击了保存*/
+            if (label6.Text == "0" )
+            {
+                MessageBox.Show("还没有计算");
+              //  this.btnCalc_Click(sender, e);
+                return;
+            }
+
+           else  if (label6.Text == null)
+            {
+                MessageBox.Show("还没有计算");
+                //this.btnCalc_Click(sender, e);
+                return;
+            }
+            else
+            {
+                /*已经计算完毕开始保存*/
+                SavaData.ShowDialog();
+                string path = SavaData.FileName;
+                if (path == "")
+                {
+                    return;
+                }
+                using (FileStream fsWrite = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    string content ="时间:" + label8.Text+ "\r\n"+ "问事:" + textBox4.Text + "\r\n" + "下卦:" + label4.Text + "\r\n" + "上卦:" + label5.Text + "\r\n" + "变爻" + label6.Text + "\r\n" + "名页:" + label7.Text;
+                    byte[] buffer = Encoding.Default.GetBytes(content);
+                    fsWrite.Write(buffer, 0, buffer.Length);
+                }
+                MessageBox.Show("保存成功");
+            }
+  
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
